8797397a839d9d89372090be0bd7a5711ef253cb
priyaRaghavan-code <61274816+priyaRaghavan-code@users.noreply.github.com>
2021-05-19 13:26:34 +0530

    Update Array_Q6.rb

--- Array_Q6.rb
+++ Array_Q6.rb
@@ -1,4 +1,16 @@
-array_one = [2, 4, 5, 7, 8, 10, 12]
-array_two = [1, 2, 3, 6, 12]
+class Common_Element
+	attr_accessor :array1
+	def initialize(array1)
+		@array1 = array1
+	end
 
-print array_one.intersection(array_two)
+	def -(obj)
+		return self.array1.intersection(obj.array1)
+	end
+
+end
+
+array_one = Common_Element.new([2, 4, 5, 7, 8, 10, 12])
+array_two = Common_Element.new([1, 2, 3, 6, 12])
+
+print array_one - array_two 
