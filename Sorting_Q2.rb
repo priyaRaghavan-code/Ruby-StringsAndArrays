163a5f673d2c34bf9d20981ae7fa0f99666b1822
priyaRaghavan-code <61274816+priyaRaghavan-code@users.noreply.github.com>
2021-05-19 12:38:59 +0530

    Delete Sorting_Q2.rb

--- Sorting_Q2.rb
+++
@@ -1,2 +0,0 @@
-hash_array =  [{ :width => 3, :height => 2}, { :width => 1, :height => 1}, { :width => 1, :height => 2 }]
-print hash_array.sort { |a, b| (a[:width] * a[:height]) - (b[:width] * b[:height]) }
