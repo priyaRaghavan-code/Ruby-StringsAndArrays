83784d2274a3c94fff4784d02b602e669ac1c5fd
Ubuntu <ubuntu@ip-10-0-0-163.us-east-2.compute.internal>
2021-05-24 15:21:53 +0000

    Output

--- ReadResume/read_pdf.rb
+++ ReadResume/read_pdf.rb
@@ -14,10 +14,10 @@ class ReadResume
       if accepted_formats.include?File.extname(file)
         reader = PDF::Reader.new(file)
         reader.pages.each do |page|
-         dictionary_file = {"file_name":file,"ocr_text":page.text}
-         dictionary_file.to_json
-         system("touch #{file}.json")
-         File.open("#{file}.json","w") { |file| file.write(dictionary_file.to_json) }
+         dictionary_file = {:file_name =>file,:ocr_text=>page.text}
+	 file.slice! ".pdf"
+	 File.write("#{file}.json",JSON.dump(dictionary_file))
+         #File.open("#{file}.json","w") { |files| files.write(dictionary_file.to_json) }
          p "completed Writing"
         end
       end
@@ -25,13 +25,19 @@ class ReadResume
   end
   def connect_with_db
     begin
-      con = PG.connect :dbname => 'resume_db', :user => 'postgres', :password => 'test'
+      con = PG::Connection.new(:dbname => 'resume_db', :user => 'postgres', :password => 'test', :port => '8082', :host =>'localhost')
+      p "DB Connected"
+      con.exec "select * from resume_table"
       accepted_formats = [".json"]
       Dir.foreach(@resume_directory) do |file|
         if accepted_formats.include?File.extname(file)
+	  p file
           file_data = File.read(file)
           file_data_in_json = JSON.parse(file_data)
-          con.exec "INSERT INTO resume_table(file_name,resume_text) values (#{file_data_in_json["file_name"]}, #{file_data_in_json["ocr_text"]})"
+	  file_name =  file_data_in_json['file_name']
+	  file_data = file_data_in_json['ocr_text']
+         # con.exec "INSERT INTO resume_table(file_name,resume_text) values (#{file_data_in_json['file_name']}, #{file_data_in_json['ocr_text']});"
+	  con.exec "INSERT INTO resume_table(file_name,resume_text) values ('#{file_name}','#{file_data}');"
           p "Inserted Successfully "+file
         end
       end
